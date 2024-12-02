28b3d464a2f52d5e670a563a52d11b0561fca5be
toneysui <charlie_5899@hotmail.com>
2024-12-01 20:49:55 -0800

    Added Chapter 1 events, and Adding tools for All Map with Notes.

--- WindingTale2/Assets/Scripts/Chapters/Chapter1.cs
+++ WindingTale2/Assets/Scripts/Chapters/Chapter1.cs
@@ -19,10 +19,10 @@ namespace WindingTale.Chapters
         {
             int eventId = 0;
             LoadTurnEvent(++eventId, 1, CreatureFaction.Friend, turn1);
-            LoadTurnEvent(++eventId, 3, CreatureFaction.Friend, turn3);
-            LoadTurnEvent(++eventId, 4, CreatureFaction.Friend, turn4);
-            LoadTurnEvent(++eventId, 5, CreatureFaction.Friend, turn5_Boss);
-            LoadTurnEvent(++eventId, 6, CreatureFaction.Friend, turn6_Npc);
+            LoadTurnEvent(++eventId, 3, CreatureFaction.Enemy, turn3);
+            LoadTurnEvent(++eventId, 4, CreatureFaction.Enemy, turn4);
+            LoadTurnEvent(++eventId, 5, CreatureFaction.Enemy, turn5_Boss);
+            LoadTurnEvent(++eventId, 6, CreatureFaction.Npc, turn6_Npc);
 
             LoadDeadEvent(++eventId, 1, (gameMain) => gameMain.onGameOver());
             LoadDeadEvent(++eventId, 5, hanuoDead);
@@ -127,18 +127,91 @@ namespace WindingTale.Chapters
 
         private Action<GameMain> turn3 = (gameMain) =>
         {
+            // Friends appear
+            FDCreature c5 = AddCreatureToMap(gameMain, CreatureFaction.Friend, 5, 5, FDPosition.At(12, 12));
+            //// FDCreature c6 = AddCreatureToMap(gameMain, CreatureFaction.Friend, 6, 1016, FDPosition.At(12, 12));
+            FDCreature c6 = AddCreatureToMap(gameMain, CreatureFaction.Friend, 6, 5, FDPosition.At(12, 12));
+
+            gameMain.PushActivity(new ParallelActivity(
+                new ActivityBase[] {
+                    ActivityFactory.CreatureWalkActivity(5, FDMovePath.Create(FDPosition.At(12, 12), FDPosition.At(12, 14))),
+                    ActivityFactory.CreatureWalkActivity(6, FDMovePath.Create(FDPosition.At(12, 12), FDPosition.At(12, 13)))
+                }
+            ));
+
+            // Talking
+            ShowConversations(gameMain, 1, 2, 1, 13);
         };
 
         private Action<GameMain> turn4 = (gameMain) =>
         {
+            gameMain.PushActivity((gameMain) =>
+            {
+                // Enemy Group1 appear
+                AddCreatureToMap(gameMain, CreatureFaction.Enemy, 21, 50101, FDPosition.At(19, 23));
+                AddCreatureToMap(gameMain, CreatureFaction.Enemy, 22, 50101, FDPosition.At(20, 22), 101);
+                AddCreatureToMap(gameMain, CreatureFaction.Enemy, 23, 50101, FDPosition.At(21, 21));
+                AddCreatureToMap(gameMain, CreatureFaction.Enemy, 24, 50101, FDPosition.At(22, 21));
+            });
+
+            gameMain.PushActivity(new ParallelActivity(
+                new ActivityBase[] {
+                    ActivityFactory.CreatureWalkActivity(21, FDMovePath.Create(FDPosition.At(19, 23), FDPosition.At(17, 23), FDPosition.At(17, 21))),
+                    ActivityFactory.CreatureWalkActivity(22, FDMovePath.Create(FDPosition.At(20, 22), FDPosition.At(20, 20), FDPosition.At(18, 20))),
+                    ActivityFactory.CreatureWalkActivity(23, FDMovePath.Create(FDPosition.At(21, 21), FDPosition.At(21, 20), FDPosition.At(20, 20))),
+                    ActivityFactory.CreatureWalkActivity(24, FDMovePath.Create(FDPosition.At(22, 21), FDPosition.At(21, 21), FDPosition.At(21, 19))),
+                }
+            ));
+
+            ShowConversations(gameMain, 1, 3, 1, 2);
         };
 
         private Action<GameMain> turn5_Boss = (gameMain) =>
         {
+            gameMain.PushActivity((gameMain) =>
+            {
+                // Enemy Group1 appear
+                AddCreatureToMap(gameMain, CreatureFaction.Enemy, 25, 50101, FDPosition.At(1, 22));
+                AddCreatureToMap(gameMain, CreatureFaction.Enemy, 26, 50101, FDPosition.At(2, 22));
+                AddCreatureToMap(gameMain, CreatureFaction.Enemy, 27, 50101, FDPosition.At(5, 24));
+                AddCreatureToMap(gameMain, CreatureFaction.Enemy, 28, 50101, FDPosition.At(5, 24));
+                AddCreatureToMap(gameMain, CreatureFaction.Enemy, 29, 50102, FDPosition.At(4, 23), 201);
+            });
+
+            gameMain.PushActivity(new ParallelActivity(
+                new ActivityBase[] {
+                    ActivityFactory.CreatureWalkActivity(29, FDMovePath.Create(FDPosition.At(4, 23), FDPosition.At(4, 22))),
+                    ActivityFactory.CreatureWalkActivity(25, FDMovePath.Create(FDPosition.At(1, 22), FDPosition.At(1, 21))),
+                    ActivityFactory.CreatureWalkActivity(26, FDMovePath.Create(FDPosition.At(2, 22), FDPosition.At(2, 21))),
+                    ActivityFactory.CreatureWalkActivity(27, FDMovePath.Create(FDPosition.At(5, 24), FDPosition.At(5, 23))),
+                    ActivityFactory.CreatureWalkActivity(28, FDMovePath.Create(FDPosition.At(5, 24), FDPosition.At(6, 24))),
+                }
+            ));
+
+            ShowConversations(gameMain, 1, 4, 1, 11);
         };
 
         private Action<GameMain> turn6_Npc = (gameMain) =>
         {
+            gameMain.PushActivity((gameMain) =>
+            {
+                // Enemy Group1 appear
+                AddCreatureToMap(gameMain, CreatureFaction.Npc, 31, 50103, FDPosition.At(24, 15));
+                AddCreatureToMap(gameMain, CreatureFaction.Npc, 32, 50103, FDPosition.At(24, 15));
+                AddCreatureToMap(gameMain, CreatureFaction.Npc, 33, 50103, FDPosition.At(24, 15));
+                AddCreatureToMap(gameMain, CreatureFaction.Npc, 34, 50103, FDPosition.At(24, 15));
+            });
+
+            gameMain.PushActivity(new ParallelActivity(
+                new ActivityBase[] {
+                    ActivityFactory.CreatureWalkActivity(31, FDMovePath.Create(FDPosition.At(24, 15), FDPosition.At(18, 15))),
+                    ActivityFactory.CreatureWalkActivity(32, FDMovePath.Create(FDPosition.At(24, 15), FDPosition.At(19, 15), FDPosition.At(19, 14))),
+                    ActivityFactory.CreatureWalkActivity(33, FDMovePath.Create(FDPosition.At(24, 15), FDPosition.At(19, 15), FDPosition.At(19, 16))),
+                    ActivityFactory.CreatureWalkActivity(34, FDMovePath.Create(FDPosition.At(24, 15), FDPosition.At(19, 15), FDPosition.At(20, 15)))
+                }
+            ));
+
+            ShowConversations(gameMain, 1, 6, 1, 5);
         };
 
         private Action<GameMain> hanuoDead = (gameMain) =>
